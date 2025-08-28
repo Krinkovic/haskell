f0737c6a9ef85568357b45dd7cc52d0b4a5e89a6
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2025-08-28 23:10:13 +0200

    completed some exercises

--- baby.hs
+++ baby.hs
@@ -1,3 +1,4 @@
+-- Learn You a Haskell
 doubleMe x = x + x
 doubleUs x y = x * 2 + y * 2
 doubleSmallNumber x = if x > 100
@@ -6,3 +7,25 @@ doubleSmallNumber x = if x > 100
 doubleSmallNumber' x = (if x > 100 then x else x * 2) + 1
 factorial n = product [1..n]
 average ns = sum ns `div` length ns
+
+-- Programming in Haskell
+-- Exercises chapter 2
+
+-- 2
+{-
+(2^3)*4
+(2*3)+(4*5)
+2+(3*(4^5))
+-}
+
+-- 3
+n = a `div` length xs
+  where
+    a = 10
+    xs = [1,2,3,4,5]
+
+-- 4
+last' xs = xs !! (length xs - 1)
+last'' xs = head (reverse xs)
+
+-- 5
